7d7b5cccddfa8c82eddb61a38e9bfd920fa16fc9
GUILHERME RAMOS TAVARES <85254258004@senacrs.edu.br>
2025-08-14 19:21:54 -0300

    meu primeiro commit

---
+++ Exercicios/ex 1.1/js/script.js
@@ -0,0 +1,15 @@
+/*
+Exercício 1.1
+Faça um programa que exiba 3 números na tela.
+
+Dica - exibindo (ou "imprimindo") números na tela:
+*/
+
+const idade= 18;
+idade >=18 ? console.log('voce é maior de idade'):console.error('voce é menor idade')
+
+const profissao = 'programador';
+profissao === 'programador'? console.log('voce é um programador'): console.error('vc não é um programador') 
+
+const trabalho = 'programador'; 
+trabalho !== 'programador'? console.log('diferente'): console.error('igual') 
